d3825555f3a82291b16f1af4afcf210cd9fd78ba
Palak-SweetCoder <pspalak6@gmail.com>
2023-01-09 14:46:21 +0600

    dashboard with two nested route added

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -20,7 +20,7 @@ const AvailableAppointments = ({ selectedDate }) => {
                 Available Services on {format(selectedDate, 'PP')}
             </h3>
             <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-20 lg:p-0 p-4">
-                {services.map((service) => (
+                {services?.map((service) => (
                     <Service
                         key={service._id}
                         service={service}
